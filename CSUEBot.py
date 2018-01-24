4c3bbde5993483ff36801315fe755fc527d3e0c3
BhavdeepSinghB <bhavdeepsmail@gmail.com>
2018-01-23 19:00:06 -0800

    Update CSUEBot.py

--- CSUEBot.py
+++ CSUEBot.py
@@ -9,14 +9,26 @@ auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
 auth.set_access_token(ACCESS_KEY, ACCESS_SECRET)
 api = tweepy.API(auth)
 
-names = api.search_users("#CSUEB")
+filename = "Users2.txt"
+fw = open(filename, "a")
+fr = open(filename, "r")
 
-filename = "UsersToFollow.txt"
-file = open(filename, "r")
+count = 0
+while(count < 5):
+    names = api.search_users("CSUEB", 20, count)
+    for auser in names:
+        fw.write("%s\n" % auser.screen_name)
+    for anotheruser in names:
+        api.create_friendship(fr.readline())
+    count = count + 1
+    print ("Number of pages covered %s" % count)
+    time.sleep(600)
 
-for auser in names:
-    api.create_friendship(file.readline())
+'''while(count <= 100):
+    api.destroy_friendship(fr.readline())
+    count = count + 1
+'''
 
-file.close()
-
+fw.close()
+fr.close()
         
